lwycool/fetchmail

commit 235ce88fdc11ec0ff3056e57004d29a3325e2b8d

Incompatible massive SSL/TLS/STARTTLS change:
Rationale: disentangle overloaded options, for clearer connection
between option name and function.

* --ssl is obsolete, and warns that --sslmode wrapped should be used.
* --sslproto got removed (due to semantic change) and got replaced by
  two new options, --sslmode and --sslprotocolversion.
* --sslmode got added and determines if/how to use SSL/TLS or STARTTLS.
* --sslprotocolversions, albeit hardly necessary, allows pinning the
  SSL/TLS version to SSLv3, TLSv1.0, or automatic selection.
* --sslcertck has become default, which causes --sslmode to silently
  upgrade from --sslmode starttls=may to --sslmode starttls=must.
* --nosslcertck was added.

imap.c
@@ -421,7 +421,7 @@ static int imap_getauth(int sock, struct query *ctl, char *greeting)
     }
 
 #ifdef SSL_ENABLE
-    if (maybe_tls(ctl)) {
+    if (maybe_starttls(ctl)) {
 	char *commonname;
 
 	commonname = ctl->server.pollname;
@@ -431,14 +431,14 @@ static int imap_getauth(int sock, struct query *ctl, char *greeting)
 	    commonname = ctl->sslcommonname;
 
 	if (strstr(capabilities, "STARTTLS")
-		|| must_tls(ctl)) /* if TLS is mandatory, ignore capabilities */
+		|| must_starttls(ctl)) /* if TLS is mandatory, ignore capabilities */
 	{
 	    /* Use "tls1" rather than ctl->sslproto because tls1 is the only
 	     * protocol that will work with STARTTLS.  Don't need to worry
 	     * whether TLS is mandatory or opportunistic unless SSLOpen() fails
 	     * (see below). */
 	    if (gen_transact(sock, "STARTTLS") == PS_SUCCESS
-		    && (set_timeout(mytimeout), SSLOpen(sock, ctl->sslcert, ctl->sslkey, "tls1", ctl->sslcertck,
+		    && (set_timeout(mytimeout), SSLOpen(sock, ctl->sslcert, ctl->sslkey, NULL, ctl->sslcertck,
 			ctl->sslcertfile, ctl->sslcertpath, ctl->sslfingerprint, commonname,
 			ctl->server.pollname, &ctl->remotename)) != -1)
 	    {
@@ -461,7 +461,7 @@ static int imap_getauth(int sock, struct query *ctl, char *greeting)
 		{
 		    report(stdout, GT_("%s: upgrade to TLS succeeded.\n"), commonname);
 		}
-	    } else if (must_tls(ctl)) {
+	    } else if (must_starttls(ctl)) {
 		/* Config required TLS but we couldn't guarantee it, so we must
 		 * stop. */
 		set_timeout(0);
@@ -477,7 +477,7 @@ static int imap_getauth(int sock, struct query *ctl, char *greeting)
 		if (gen_transact(sock, "NOOP") != PS_SUCCESS) {
 		    /* Not usable.  Empty sslproto to force an unencrypted
 		     * connection on the next attempt, and repoll. */
-		    ctl->sslproto = xstrdup("");
+		    ctl->sslmode = TLSM_NONE;
 		    return PS_REPOLL;
 		}
 		/* Usable.  Proceed with authenticating insecurely. */

pop3.c
@@ -436,7 +436,7 @@ static int pop3_getauth(int sock, struct query *ctl, char *greeting)
 		(ctl->server.authenticate == A_KERBEROS_V5) ||
 		(ctl->server.authenticate == A_OTP) ||
 		(ctl->server.authenticate == A_CRAM_MD5) ||
-		maybe_tls(ctl))
+		maybe_starttls(ctl))
 	{
 	    if ((ok = capa_probe(sock)) != PS_SUCCESS)
 		/* we are in STAGE_GETAUTH => failure is PS_AUTHFAIL! */
@@ -449,15 +449,14 @@ static int pop3_getauth(int sock, struct query *ctl, char *greeting)
 		    (ok == PS_SOCKET && !ctl->wehaveauthed))
 		{
 #ifdef SSL_ENABLE
-		    if (must_tls(ctl)) {
+		    if (must_starttls(ctl)) {
 			/* fail with mandatory STLS without repoll */
 			report(stderr, GT_("TLS is mandatory for this session, but server refused CAPA command.\n"));
 			report(stderr, GT_("The CAPA command is however necessary for TLS.\n"));
 			return ok;
-		    } else if (maybe_tls(ctl)) {
+		    } else if (maybe_starttls(ctl)) {
 			/* defeat opportunistic STLS */
-			xfree(ctl->sslproto);
-			ctl->sslproto = xstrdup("");
+			ctl->sslmode = TLSM_NONE;
 		    }
 #endif
 		    /* If strong authentication was opportunistic, retry without, else fail. */
@@ -474,7 +473,7 @@ static int pop3_getauth(int sock, struct query *ctl, char *greeting)
 	}
 
 #ifdef SSL_ENABLE
-	if (maybe_tls(ctl)) {
+	if (maybe_starttls(ctl)) {
 	    char *commonname;
 
 	    commonname = ctl->server.pollname;
@@ -484,14 +483,14 @@ static int pop3_getauth(int sock, struct query *ctl, char *greeting)
 		commonname = ctl->sslcommonname;
 
 	   if (has_stls
-		   || must_tls(ctl)) /* if TLS is mandatory, ignore capabilities */
+		   || must_starttls(ctl)) /* if TLS is mandatory, ignore capabilities */
 	   {
 	       /* Use "tls1" rather than ctl->sslproto because tls1 is the only
 		* protocol that will work with STARTTLS.  Don't need to worry
 		* whether TLS is mandatory or opportunistic unless SSLOpen() fails
 		* (see below). */
 	       if (gen_transact(sock, "STLS") == PS_SUCCESS
-		       && (set_timeout(mytimeout), SSLOpen(sock, ctl->sslcert, ctl->sslkey, "tls1", ctl->sslcertck,
+		       && (set_timeout(mytimeout), SSLOpen(sock, ctl->sslcert, ctl->sslkey, NULL, ctl->sslcertck,
 			   ctl->sslcertfile, ctl->sslcertpath, ctl->sslfingerprint, commonname,
 			   ctl->server.pollname, &ctl->remotename)) != -1)
 	       {
@@ -518,7 +517,7 @@ static int pop3_getauth(int sock, struct query *ctl, char *greeting)
 		   {
 		       report(stdout, GT_("%s: upgrade to TLS succeeded.\n"), commonname);
 		   }
-	       } else if (must_tls(ctl)) {
+	       } else if (must_starttls(ctl)) {
 		   /* Config required TLS but we couldn't guarantee it, so we must
 		    * stop. */
 		   set_timeout(0);
@@ -538,7 +537,7 @@ static int pop3_getauth(int sock, struct query *ctl, char *greeting)
 		   }
 	       }
 	   }
-	} /* maybe_tls() */
+	} /* maybe_starttls() */
 #endif /* SSL_ENABLE */
 
 	/*
@@ -670,8 +669,7 @@ static int pop3_getauth(int sock, struct query *ctl, char *greeting)
     if (connection_may_have_tls_errors
 		    && (ok == PS_SOCKET || ok == PS_PROTOCOL))
     {
-	xfree(ctl->sslproto);
-	ctl->sslproto = xstrdup("");
+	ctl->sslmode = TLSM_NONE;
 	/* repoll immediately without TLS */
 	ok = PS_REPOLL;
     }

TODO-7.0
@@ -31,7 +31,6 @@ and 8.0 for C++
 SSL fixes:
 - write .man documentation, adjust FAQ (check diffs for what needs to be
   documented)
-- debug sslcertck default (gets disabled somehow)
 
 Debian #632479 - doesn't accept -f and --pidfile options when waking up
 BG daemon (watch out for idfile though!)

conf.c
@@ -371,10 +371,10 @@ void dump_config(struct runctl *runp, struct query *querylist)
 	numdump("fastuidl", ctl->fastuidl);
 	numdump("batchlimit", ctl->batchlimit);
 #ifdef SSL_ENABLE
-	booldump("ssl", ctl->use_ssl);
 	stringdump("sslkey", ctl->sslkey);
 	stringdump("sslcert", ctl->sslcert);
-	stringdump("sslproto", ctl->sslproto);
+	stringdump("sslmode", tlsm_string(ctl->sslmode));
+	stringdump("sslprotocolversion", ctl->sslproto);
 	booldump("sslcertck", ctl->sslcertck);
 	stringdump("sslcertpath", ctl->sslcertpath);
 	stringdump("sslcommonname", ctl->sslcommonname);

configure.ac
@@ -506,7 +506,7 @@ AC_ARG_WITH(ssl,
 	[  --with-ssl=[DIR]        enable SSL support using libraries in DIR],
 	[with_ssl=$withval],
 	[with_ssl=yes])
-test "$with_ssl" != "no" && AC_DEFINE(SSL_ENABLE,1,Define if you want SSL support compiled in)
+	[with_ssl=yes])
 
 if test "$with_ssl" = "yes"
 then
@@ -575,7 +575,7 @@ then
   if test "$i" = "" ; then i="(none)" ; fi
   AC_MSG_RESULT($i)
   dnl XXX FIXME: use pkg-config if available!
-  AC_DEFINE(SSL_ENABLE)
+  AC_DEFINE(SSL_ENABLE, 1, [Define if you want SSL support compiled in])
 else
   AC_MSG_WARN(Disabling SSL support.)
   AC_MSG_WARN(Consider re-running configure --with-ssl.)

driver.c
@@ -1015,7 +1015,7 @@ static int do_session(
 	if (ctl->server.plugin)
 	    (void)sleep(1);
 	if ((mailserver_socket = SockOpen(realhost, 
-			     ctl->server.service ? ctl->server.service : ( ctl->use_ssl ? ctl->server.base_protocol->sslservice : ctl->server.base_protocol->service ),
+			     ctl->server.service ? ctl->server.service : ( ctl->sslmode == TLSM_WRAPPED ? ctl->server.base_protocol->sslservice : ctl->server.base_protocol->service ),
 			     ctl->server.plugin, &ai0)) == -1)
 	{
 	    char	errbuf[BUFSIZ];
@@ -1047,7 +1047,7 @@ static int do_session(
 	set_timeout(mytimeout);
 
 	/* perform initial SSL handshake on open connection */
-	if (ctl->use_ssl &&
+	if (ctl->sslmode == TLSM_WRAPPED &&
 		SSLOpen(mailserver_socket, ctl->sslcert, ctl->sslkey,
 		    ctl->sslproto, ctl->sslcertck,
 		    ctl->sslcertfile, ctl->sslcertpath,

fetchmail-FAQ.html
@@ -2127,18 +2127,19 @@ SSL?</a></h2>
 <p>You'll need to have the <a
 href="http://www.openssl.org/">OpenSSL</a> libraries installed, and they
 should at least be version 0.9.7.
-Configure with --with-ssl. If you have the OpenSSL libraries
-installed in commonly-used default locations, this will
-suffice. If you have them installed in a non-default location,
+Configure with --with-ssl (default since fetchmail v7.0.0). If you
+have the OpenSSL libraries installed in commonly-used default locations,
+this will suffice. If you have them installed in a non-default location,
 you'll need to specify the OpenSSL installation directory as an argument
 to --with-ssl after an equal sign.</p>
 
-<p>Fetchmail binaries built this way support <code>ssl</code>,
-<code>sslkey</code>, and <code>sslcert</code> options that control
-SSL encryption, and will automatically use <code>tls</code> if the
-server offers it. You will need to have an SSL-enabled mailserver to
-use these options. See the manual page for details and some words
-of care on the limited security provided.</p>
+<p>Fetchmail binaries built this way supports various options all starting with <code>ssl</code>
+that control SSL encryption, and will automatically use <code>STARTTLS</code>
+if the server offers it. v7.0.0 and newer fetchmail versions will
+automatically fail (unless overridden) if the certificate cannot be
+validated. You will need to have an SSL-enabled mailserver to use these
+options. See the manual page for details and some words of care on the
+limited security provided.</p>
 
 <p>If your open OpenSSL session dies with a message that complains
 "PRNG not seeded", update or improve your operating system. This

fetchmail.c
@@ -74,6 +74,8 @@ static int activecount;		/* count number of active entries */
 static struct runctl cmd_run;	/* global options set from command line */
 static time_t parsetime;	/* time of last parse */
 
+struct query cmd_opts;		/* where to put command-line info */
+
 static void terminate_run(int);
 static void terminate_poll(int);
 
@@ -1304,9 +1306,9 @@ static void optmerge(struct query *h2, struct query *h1, int force)
     FLAG_MERGE(fastuidl);
     FLAG_MERGE(batchlimit);
 #ifdef	SSL_ENABLE
-    FLAG_MERGE(use_ssl);
     FLAG_MERGE(sslkey);
     FLAG_MERGE(sslcert);
+    FLAG_MERGE(sslmode);
     FLAG_MERGE(sslproto);
     FLAG_MERGE(sslcertck);
     FLAG_MERGE(sslcertfile);
@@ -1348,6 +1350,10 @@ static int load_params(int argc, char **argv, int optind)
     def_opts.listener = SMTP_MODE;
     def_opts.fetchsizelimit = 100;
     def_opts.fastuidl = 4;
+#ifdef SSL_ENABLE
+    def_opts.sslcertck = FLAG_TRUE;
+    def_opts.sslmode = TLSM_STLS_MAY;
+#endif
 
     /* get the location of rcfile */
     rcfiledir[0] = 0;
@@ -1621,15 +1627,14 @@ static int load_params(int argc, char **argv, int optind)
 	    DEFAULT(ctl->mimedecode, FALSE);
 	    DEFAULT(ctl->idle, FALSE);
 	    DEFAULT(ctl->server.dns, TRUE);
-	    DEFAULT(ctl->use_ssl, FALSE);
-	    DEFAULT(ctl->sslcertck, FALSE);
+	    DEFAULT(ctl->sslcertck, TRUE);
 	    DEFAULT(ctl->server.checkalias, FALSE);
 #ifndef SSL_ENABLE
 	    /*
 	     * XXX FIXME: do we need this check or can we rely on the .y
 	     * parser handling this?
 	     */
-	    if (ctl->use_ssl) 
+	    if (ctl->sslmode != TLSM_NONE)
 	    {
 		report(stderr, GT_("SSL support is not compiled in.\n"));
 		exit(PS_SYNTAX);
@@ -2021,10 +2026,10 @@ static void dump_params (struct runctl *runp,
 	if (ctl->server.principal != (char *) NULL)
 	    printf(GT_("  Mail service principal is: %s\n"), ctl->server.principal);
 #ifdef	SSL_ENABLE
-	if (ctl->use_ssl)
-	    printf(GT_("  SSL encrypted sessions enabled.\n"));
+	if (ctl->sslmode)
+	    printf(GT_("  SSL mode: %s.\n"), tlsm_string(ctl->sslmode));
 	if (ctl->sslproto)
-	    printf(GT_("  SSL protocol: %s.\n"), ctl->sslproto);
+	    printf(GT_("  SSL protocol version: %s.\n"), ctl->sslproto);
 	if (ctl->sslcertck) {
 	    printf(GT_("  SSL server certificate checking enabled.\n"));
 	}

commit fad86a3ea6d23f7a3a5216b29a500065bea6a16b

Fix regression: STARTTLS uses protocol version again.
alpha5 passed NULL rather than ctl->sslproto, causing STARTTLS-based
encryption to always auto-negotiate the SSL/TLS protocol version.

Regression in 235ce88fdc11ec0ff3056e57004d29a3325e2b8d.

imap.c
@@ -433,12 +433,8 @@ static int imap_getauth(int sock, struct query *ctl, char *greeting)
 	if (strstr(capabilities, "STARTTLS")
 		|| must_starttls(ctl)) /* if TLS is mandatory, ignore capabilities */
 	{
-	    /* Use "tls1" rather than ctl->sslproto because tls1 is the only
-	     * protocol that will work with STARTTLS.  Don't need to worry
-	     * whether TLS is mandatory or opportunistic unless SSLOpen() fails
-	     * (see below). */
 	    if (gen_transact(sock, "STARTTLS") == PS_SUCCESS
-		    && (set_timeout(mytimeout), SSLOpen(sock, ctl->sslcert, ctl->sslkey, NULL, ctl->sslcertck,
+		    && (set_timeout(mytimeout), SSLOpen(sock, ctl->sslcert, ctl->sslkey, ctl->sslproto, ctl->sslcertck,
 			ctl->sslcertfile, ctl->sslcertpath, ctl->sslfingerprint, commonname,
 			ctl->server.pollname, &ctl->remotename)) != -1)
 	    {

pop3.c
@@ -485,12 +485,8 @@ static int pop3_getauth(int sock, struct query *ctl, char *greeting)
 	   if (has_stls
 		   || must_starttls(ctl)) /* if TLS is mandatory, ignore capabilities */
 	   {
-	       /* Use "tls1" rather than ctl->sslproto because tls1 is the only
-		* protocol that will work with STARTTLS.  Don't need to worry
-		* whether TLS is mandatory or opportunistic unless SSLOpen() fails
-		* (see below). */
 	       if (gen_transact(sock, "STLS") == PS_SUCCESS
-		       && (set_timeout(mytimeout), SSLOpen(sock, ctl->sslcert, ctl->sslkey, NULL, ctl->sslcertck,
+		       && (set_timeout(mytimeout), SSLOpen(sock, ctl->sslcert, ctl->sslkey, ctl->sslproto, ctl->sslcertck,
 			   ctl->sslcertfile, ctl->sslcertpath, ctl->sslfingerprint, commonname,
 			   ctl->server.pollname, &ctl->remotename)) != -1)
 	       {